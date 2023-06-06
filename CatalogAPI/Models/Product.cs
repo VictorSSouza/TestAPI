ec96899bf95273be4a020c1e7713b80fbae66aa6
Victor <santosfranciscosouza310@gmail.com>
2023-06-06 11:28:48 -0300

    Add product validate

--- CatalogAPI/Models/Product.cs
+++ CatalogAPI/Models/Product.cs
@@ -1,10 +1,10 @@
-ï»¿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace CatalogAPI.Models
 {
-    public class Product
+    public class Product : IValidatableObject
     {
         public int Id { get; set; }
 
@@ -29,5 +29,28 @@ namespace CatalogAPI.Models
 
         [JsonIgnore]
         public Category? Category { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.Name))
+            {
+                string firstLetter = this.Name[0].ToString();
+                if(firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new
+                        ValidationResult("A primeira letra do produto deve ser maiúscula!",
+                        new[] { nameof(this.Name) }
+                        );
+                }
+            }
+
+            if(this.Amount <= 0)
+            {
+                yield return new
+                        ValidationResult("O estoque do produto deve ser maior que zero!",
+                        new[] { nameof(this.Amount) }
+                        );
+            }
+        }
     }
 }
